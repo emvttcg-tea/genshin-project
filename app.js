54ab52c2ca9d9eda286d2ca8291c43b6b95a3ab9
Young Tea <74453975+young-tea@users.noreply.github.com>
2023-03-29 02:23:45 +0300

    b

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
 
-
-app.get('/', function(req, res){
-    return res.send('Hello!')
+app.get('/', function (req, res) {
+  return res.send('Hello!')
 })
 
-
-app.listen(port, function() {
-    console.log(`listenning to ${port}...`);
+app.listen(port, function () {
+  console.log(`listenning to ${port}...`)
 })
