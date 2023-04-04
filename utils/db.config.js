df3ffb4539b9bb21c1172d8902e092508353c134
Young Tea <74453975+young-tea@users.noreply.github.com>
2023-04-05 02:26:31 +0300

    fcf

--- utils/db.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/genshin-master', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-
-mongoose.connection.once('open', () => {
-  console.log('connected to db')
-})
