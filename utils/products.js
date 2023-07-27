7aa1f3c6ebbcaa682fa104370fe1e5e20c27ef90
Young Tea <74453975+young-tea@users.noreply.github.com>
2023-07-28 02:59:20 +0300

    g

--- utils/products.js
+++ utils/products.js
@@ -2,11 +2,24 @@ const mongoose = require('mongoose')
 const db = mongoose.connection
 const productm = require('../modules/products/models/product')
 
-//var products = []
+// var products = {}
 
-productm.find({}).then((result) => {
-  const products = result
-})
+// productm.find({}).then((result) => {
+//   products = result
+// })
+
+var products = await productm.find({})
+
+
+// const getArrayOfData = () => {
+//   return productm.find({}).then((storedDataArray) => {
+//       return storedDataArray
+//   }).catch(function(err){
+//       if (err) {
+//           throw new Error(err.message)
+//       }
+//   })
+// }
 
 console.log(products)
 
