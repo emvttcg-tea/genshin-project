552c270dbd674169fe9de0af21b97d28fe4d414b
Young Tea <74453975+young-tea@users.noreply.github.com>
2023-07-30 06:03:59 +0300

    product page

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -12,8 +12,20 @@ router.get('/products', (req, res) => {
   productm.find({}).then((result) => {
     //console.log(result)
     
-    res.render('products', {title: 'Products - GenshinMaster', products: result})
+    res.render('products/products', {title: 'Products - GenshinMaster', products: result})
   
+  })
+
+})
+
+// product page
+router.get('/products/:id', (req, res) => {
+  console.log(req.params.id)
+
+  productm.findById(req.params.id).then((result) => {
+    console.log(result)
+
+    res.render('products/product-page', {title: `${result.itemname} - GenshinMaster`, product: result})
   })
 
 })
